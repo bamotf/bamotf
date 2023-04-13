eb533dd7c267cc1660a102e6faebc450fb9a0c9a
Wlad Paiva <desenvolvedormg@gmail.com>
2023-04-13 11:49:03 -0300

    add logger

---
+++ packages/logger/index.ts
@@ -0,0 +1,34 @@
+import winston from "winston";
+import format from "picocolors";
+
+// Create a Winston logger - passing in the Logtail transport
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV === "production") {
+  // If we're in production, we'll want to send our logs to an external service
+  // like Logtail or Datadog. To do that, we'll need to install the transport
+  // and create a client.
+  // import {Logtail} from '@logtail/node'
+  // import {LogtailTransport} from '@logtail/winston'
+  // const logtail = new Logtail(env.LOGTAIL_SOURCE_TOKEN)
+  // logger.add(new LogtailTransport(logtail))
+}
+
+export {
+  /**
+   * A logger tool that can be used to log messages to the logger service
+   */
+  logger,
+
+  /**
+   * A terminal output formatting with ANSI colors
+   */
+  format,
+};
