6b50391a8790bca3d5404c5d2e3d7fe240c79b23
Wlad Paiva <desenvolvedormg@gmail.com>
2023-04-30 12:37:41 -0300

    Add prisma zod type validation

--- apps/cashier-server/api/schemas/payment-intent.schema.ts
+++ apps/cashier-server/api/schemas/payment-intent.schema.ts
@@ -1,28 +1,29 @@
+import {Schema} from 'zod-prisma-utils'
 import {z} from '../utils/zod'
 
 import {AddressSchema} from './address.schema'
 import {AmountSchema} from './amount.schema'
+import type {PaymentIntent} from 'db'
 
-export const PaymentIntentSchema = z.object(
-  // <
-  //   Schema<PaymentIntent, HiddenFields>
-  // >
-  {
-    id: z.string(),
-    /**
-     * @type {string} - The address the user needs to pay to
-     */
-    address: z.string(),
-    /**
-     * @type {bigint} - The amount the user needs to pay in satoshis
-     */
-    amount: AmountSchema,
-    /**
-     * @type {bigint} - The amount the user has already payed in satoshis
-     */
-    amountReceived: AmountSchema,
-    description: z.string().optional(),
-    status: z.enum(['CONFIRMING', 'CANCELED', 'SUCCEEDED']),
-    cancellationReason: z.string().optional(),
-  },
-)
+type SchemaType = Schema<PaymentIntent, 'createdAt' | 'updatedAt'>
+
+export const PaymentIntentSchema = z.object({
+  id: z.string(),
+  /**
+   * @type {string} - The address the user needs to pay to
+   */
+  address: z.string(),
+  /**
+   * @type {bigint} - The amount the user needs to pay in satoshis
+   */
+  amount: AmountSchema,
+  /**
+   * @type {bigint} - The amount the user has already payed in satoshis
+   */
+  amountReceived: AmountSchema,
+  description: z.string().nullish(),
+  status: z.enum(['CONFIRMING', 'CANCELED', 'SUCCEEDED']),
+  canceledAt: z.date().optional(),
+  cancellationReason: z.string().optional(),
+  accountId: z.string().uuid().optional(),
+} satisfies SchemaType)
