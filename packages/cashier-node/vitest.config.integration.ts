55207cb100c32670f0a4cfe5b4e5918e17bc6894
Wlad Paiva <desenvolvedormg@gmail.com>
2023-05-22 16:42:07 -0300

    chore: update tests

--- packages/cashier-node/vitest.config.integration.ts
+++ packages/cashier-node/vitest.config.integration.ts
@@ -3,6 +3,6 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['src/**/*.test.ts'],
-    setupFiles: ['./tests/setup.unit.ts'],
+    // setupFiles: ['./tests/setup.unit.ts'],
   },
 })
