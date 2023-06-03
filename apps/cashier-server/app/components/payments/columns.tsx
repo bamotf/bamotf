ad626d757f6b2aff861db02530280e0554e76441
Wlad Paiva <desenvolvedormg@gmail.com>
2023-06-03 17:50:22 -0300

    format column

--- apps/cashier-server/app/components/payments/columns.tsx
+++ apps/cashier-server/app/components/payments/columns.tsx
@@ -3,16 +3,29 @@ import type {ColumnDef} from '@tanstack/react-table'
 
 export const columns: ColumnDef<PaymentIntent>[] = [
   {
-    accessorKey: 'status',
-    header: 'Status',
+    accessorKey: 'amount',
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({row}) => {
+      const amount = parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumSignificantDigits: 8,
+        // minimumSignificantDigits: 0,
+      })
+        .format(amount * 1e-8)
+        .replace('$', '₿')
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
   },
   {
     accessorKey: 'address',
     header: 'Address',
   },
   {
-    accessorKey: 'amount',
-    header: 'Amount',
+    accessorKey: 'status',
+    header: 'Status',
   },
   {
     accessorKey: 'createdAt',
