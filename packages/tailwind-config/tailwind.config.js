04be33829a4998d63210b8fb332de023b38ff0d4
Wlad Paiva <desenvolvedormg@gmail.com>
2023-06-24 06:50:14 -0300

    config shadcn ui

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['app/**/*.{ts,tsx}', `src/**/*.{ts,tsx}`],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
