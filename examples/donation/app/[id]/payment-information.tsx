b64f5337698f658adcffe078a96f6d6b001c762b
Ralf Dewrich <ralfferreira995@gmail.com>
2023-06-27 14:55:58 -0300

    QRCode component receiving address, amount, label and message, URL format correctly

--- examples/donation/app/[id]/payment-information.tsx
+++ examples/donation/app/[id]/payment-information.tsx
@@ -7,16 +7,31 @@ export async function PaymentInformation({
   currency,
   status,
   address,
+  label,
+  message,
   redirectUrl,
 }: {
   amount: number
   address: string
   status: PaymentIntentStatus
   currency: string
+  label?: string
+  message?: string
   redirectUrl: string
 }) {
   const response = await fetch(`http://localhost:3000/api/price/${currency}`)
   const {price} = await response.json()
+
+  // Be like: parseFloat(currency.toFraction(amount, 8))
+  const btcAmount = Number(amount) / 100000000 // Convert satoshis to BTC with 8 decimal places
+
+  const urlParams = new URLSearchParams({
+    amount: btcAmount.toString(),
+    label: label ? encodeURIComponent(label) : '',
+    message: message ? encodeURIComponent(message) : '',
+  })
+
+  const qrCodeValue = `bitcoin:${address}?${urlParams.toString()}&r=${redirectUrl}/success`
 
   return (
     <>
@@ -28,11 +43,7 @@ export async function PaymentInformation({
         Amount to pay: {Math.ceil((amount / price) * 1e8) / 1e8}
       </div>
 
-      <QRCodeSVG
-        bgColor="#FFFFFF00"
-        fgColor="#FFFFFFF0"
-        value={`bitcoin:${address}?amount=${amount}&r=${redirectUrl}/success`}
-      />
+      <QRCodeSVG bgColor="#FFFFFF00" fgColor="#FFFFFFF0" value={qrCodeValue} />
       {address}
     </>
   )
