f8799fa8d6a115e511a2a8c9667026c7ac5ecf67
Ralf Dewrich <ralfferreira995@gmail.com>
2023-06-29 18:48:59 -0300

    Separate payment-information and payment-intent

--- examples/donation/app/[id]/payment-information.tsx
+++ examples/donation/app/[id]/payment-information.tsx
@@ -1,28 +1,7 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import type {PaymentIntentStatus} from '@bam-otf/node'
-import {QRCodeSVG} from 'qrcode.react'
 
-interface CopyButtonProps {
-  text: string
-}
-
-function CopyButton({text}: CopyButtonProps) {
-  const [copied, setCopied] = useState(false)
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(text)
-    setCopied(true)
-    setTimeout(() => {
-      setCopied(false)
-    }, 5000)
-  }
-
-  return (
-    <span onClick={handleCopy} className="cursor-pointer">
-      {copied ? 'Copied! ✅' : 'Copy'}
-    </span>
-  )
-}
+import {PaymentIntent} from './payment-intent'
 
 export async function PaymentInformation({
   amount,
@@ -41,46 +20,13 @@ export async function PaymentInformation({
   message?: string
   redirectUrl: string
 }) {
-  // if (currency !== 'BTC') {
-  //   throw new Error('Invalid payment intent currency. Only BTC is supported.')
-  // }
-
   const response = await fetch(`http://localhost:3000/api/price/${currency}`)
   const {price} = await response.json()
 
-  const btcAmount = Math.ceil((amount / price) * 1e8) / 1e8 // Convert satoshis to BTC with 8 decimal places
-
-  const urlParams = new URLSearchParams({
-    amount: btcAmount.toString(),
-    label: label ? encodeURIComponent(label) : '',
-    message: message ? encodeURIComponent(message) : '',
-  })
-
-  const qrCodeValue = `bitcoin:${address}?${urlParams.toString()}&r=${redirectUrl}/success`
-
   return (
-    <>
-      <div className="">
-        Donating: {amount} {currency}
-      </div>
-      <div className="">BTC PRICE: {price}</div>
-      <div className="">Amount to pay: {btcAmount}</div>
-
-      <QRCodeSVG bgColor="#FFFFFF00" fgColor="#FFFFFFF0" value={qrCodeValue} />
-
-      <div className="">
-        Address:
-        <div>
-          {address} <CopyButton text={address} />
-        </div>
-      </div>
-
-      <div className="">
-        Amount in BTC:
-        <div>
-          {btcAmount} <CopyButton text={btcAmount.toString()} />
-        </div>
-      </div>
-    </>
+    <PaymentIntent
+      intent={{amount, currency, status, address, label, message, redirectUrl}}
+      price={price}
+    />
   )
 }
