b086c98aa0c0716528c8ebc097aae100da437fe6
Wlad Paiva <desenvolvedormg@gmail.com>
2023-07-04 16:27:25 -0300

    improve simulator

--- apps/bam-otf-server/app/routes/simulate.tsx
+++ apps/bam-otf-server/app/routes/simulate.tsx
@@ -1,34 +1,114 @@
+import {useEffect, useState} from 'react'
+import {conform, useForm} from '@conform-to/react'
+import {getFieldsetConstraint, parse} from '@conform-to/zod'
 import {json, type ActionArgs} from '@remix-run/node'
-import {Form} from '@remix-run/react'
+import {Form, useActionData, useNavigation} from '@remix-run/react'
 
-import {Input} from '~/components/ui/input'
-import {PaymentIntentSchema} from '~/schemas'
+import {Button, ErrorList, Field} from '~/components/forms'
 import {simulatePayment} from '~/utils/bitcoin-core'
-import {createContract} from '~/utils/contract'
+import {z} from '~/utils/zod'
 
-export const contract = createContract({
-  action: {
-    body: PaymentIntentSchema.pick({
-      amount: true,
-      address: true,
-    }),
-  },
+const schema = z.object({
+  amount: z.coerce.number(),
+  address: z.string().min(1, 'Address is required'),
 })
 
 export async function action({request}: ActionArgs) {
-  const {body} = await contract.action({request})
-  const result = await simulatePayment(body)
-  return json(result)
+  const formData = await request.formData()
+  const submission = parse(formData, {schema})
+
+  if (!submission.value || submission.intent !== 'submit') {
+    return json(submission, {status: 400})
+  }
+
+  await simulatePayment(submission.value)
+
+  return json(submission, {status: 200})
 }
 
-export default function SimulatePage() {
+export default function SimulateForm({
+  redirectTo,
+  formError,
+}: {
+  redirectTo?: string
+  formError?: string | null
+}) {
+  const [status, setStatus] = useState<
+    'pending' | 'idle' | 'error' | 'success'
+  >('idle')
+
+  const lastSubmission = useActionData<typeof action>()
+
+  const [form, fields] = useForm({
+    id: 'inline-login',
+    defaultValue: {redirectTo},
+    constraint: getFieldsetConstraint(schema),
+    lastSubmission,
+    shouldRevalidate: 'onBlur',
+  })
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    if (navigation.state === 'submitting') {
+      setStatus('pending')
+    }
+
+    if (lastSubmission?.error) {
+      setStatus('error')
+    }
+
+    if (lastSubmission?.value) {
+      setStatus('success')
+    }
+
+    let timeout = setTimeout(() => {
+      setStatus('idle')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [navigation, lastSubmission])
+
   return (
-    <Form method="post">
-      Amount
-      <Input type="text" name="amount" />
-      address
-      <Input type="text" name="address" />
-      <button type="submit">Submit</button>
-    </Form>
+    <div>
+      <div className="mx-auto w-full max-w-md px-8">
+        <Form method="POST" {...form.props}>
+          <Field
+            className="mb-4"
+            labelProps={{
+              htmlFor: fields.address.id,
+              children: 'Address',
+            }}
+            inputProps={{...conform.input(fields.address), autoFocus: true}}
+            errors={fields.address.errors}
+          />
+
+          <Field
+            className="mb-4"
+            labelProps={{
+              htmlFor: fields.amount.id,
+              children: 'Amount',
+            }}
+            inputProps={conform.input(fields.amount)}
+            errors={fields.amount.errors}
+          />
+
+          <ErrorList errors={[...form.errors, formError]} id={form.errorId} />
+
+          <div className="flex items-center justify-between gap-6 pt-3">
+            <Button
+              className="w-full"
+              status={status}
+              type="submit"
+              disabled={navigation.state !== 'idle'}
+            >
+              Pay
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
   )
 }
