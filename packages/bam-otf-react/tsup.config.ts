aaf01f3581c1c57bc3c02acbeb268713f6741fde
Wlad Paiva <desenvolvedormg@gmail.com>
2023-07-04 16:33:40 -0300

    add sourcemap for pinned entry points

--- packages/bam-otf-react/tsup.config.ts
+++ packages/bam-otf-react/tsup.config.ts
@@ -3,11 +3,12 @@ import {defineConfig, type Options} from 'tsup'
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ['src/**/*.tsx'],
+  entry: ['src/index.tsx', 'src/styles.tsx'],
   format: ['esm', 'cjs'],
   dts: true,
   minify: true,
   clean: true,
   external: ['react'],
+  sourcemap: true,
   ...options,
 }))
