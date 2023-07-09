61b592777dd58202c8677a7d8df78ab3e7381b97
Wlad Paiva <desenvolvedormg@gmail.com>
2023-07-09 20:55:13 -0300

    @bam-otf/react (#25)
    * create sharable react components
    
    * Add a hack to resolve BigInt bug
    
    * Add storybook components and delete storybook component examples
    
    * Styling react components, I have not finished
    
    * Add Open in wallet button, add stories to component
    
    * Styling components
    
    * Styling QRCode component and modify buttons size, add responsive
    
    * Modify url and delete unused files
    
    * Add currency selector structure
    
    * Add currency selector component, add stories to currency
    
    * Add three backgrounds and modify component
    
    * Add three backgrounds to stories
    
    * remove stuff
    
    * create base style & style qrcode
    
    * style copyable
    
    * remove unused copyable components
    
    * style payment-details
    
    * Adding PR suggestions
    
    ---------
    
    Co-authored-by: Ralf Dewrich <ralfferreira995@gmail.com>

--- packages/bam-otf-react/tsup.config.ts
+++ packages/bam-otf-react/tsup.config.ts
@@ -3,11 +3,12 @@ import {defineConfig, type Options} from 'tsup'
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ['src/**/*.tsx'],
+  entry: ['src/index.ts', 'src/styles.tsx'],
   format: ['esm', 'cjs'],
-  dts: true,
+  // dts: true,
   minify: true,
   clean: true,
   external: ['react'],
+  sourcemap: 'inline',
   ...options,
 }))
