1cb93a68cf92b2ed195c8a936a5a00bafc1dc5b3
Ralf Dewrich <ralfferreira995@gmail.com>
2023-07-15 00:35:42 -0300

    Add landing background and responsive sizes

--- apps/docs/components/landing/index.tsx
+++ apps/docs/components/landing/index.tsx
@@ -2,31 +2,36 @@ import React from 'react'
 import Link from 'next/link'
 import Typed from 'react-typed'
 
-import styles from './index.module.css'
+import LandingBackground from '../landing-background/background'
 
 function LandingPage() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.landingContainer}>
-        <h1 className={styles.heading}>Bam Makes It</h1>
-        <Typed
-          className={styles.typed}
-          strings={['Fast', 'Simple', 'Easy']}
-          typeSpeed={120}
-          backSpeed={140}
-          loop
-        />
-        <p className={styles.description}>
-          Designed specifically for web developers, the best way to create
-          payment intents.
-        </p>
-      </div>
-      <p className={styles.button}>
-        <Link className={styles.cta} href="/docs">
-          Get started <span>→</span>
-        </Link>
-      </p>
-    </div>
+    <>
+      <main className="flex flex-col items-center justify-center w-full h-full">
+        <LandingBackground />
+        <div className="wrapper">
+          <div className="landingContainer">
+            <h1 className="heading">Bam Makes It</h1>
+            <Typed
+              className="typed"
+              strings={['Fast', 'Simple', 'Easy', 'Secure', 'Powerful']}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
+            <p className="description">
+              Designed specifically for web developers, the best way to create
+              payment intents.
+            </p>
+          </div>
+          <p className="button">
+            <Link className="button-link" href="/docs">
+              Get started <span>→</span>
+            </Link>
+          </p>
+        </div>
+      </main>
+    </>
   )
 }
 
