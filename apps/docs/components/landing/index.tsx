f0ea47dbbc493081bc7eda3aa9f571fcc5432d15
Ralf Dewrich <ralfferreira995@gmail.com>
2023-07-18 10:21:15 -0300

    Add comment with content on the landing page, but it is only the text

--- apps/docs/components/landing/index.tsx
+++ apps/docs/components/landing/index.tsx
@@ -8,10 +8,12 @@ function LandingPage() {
   return (
     <>
       <main className="flex flex-col items-center justify-center w-full h-full">
-        <LandingBackground />
         <div className="wrapper">
+          <LandingBackground />
           <div className="landingContainer">
-            <h1 className="heading">Bam Makes It</h1>
+            <h1 className="heading">
+              The Bitcoin toolkit <br /> for developers
+            </h1>
             <Typed
               className="typed"
               strings={['Fast', 'Simple', 'Easy', 'Secure', 'Powerful']}
@@ -20,8 +22,8 @@ function LandingPage() {
               loop
             />
             <p className="description">
-              Designed specifically for web developers, the best way to create
-              payment intents.
+              A streamlined, efficient, and open source way to integrate Bitcoin
+              as payment method.
             </p>
           </div>
           <p className="button">
@@ -35,6 +37,42 @@ function LandingPage() {
             </Link>
           </p> */}
         </div>
+
+        <section className="flex flex-col items-center justify-between w-1/2 h-screen text-center">
+          <div>
+            <h1>Why bamotf?</h1>
+            <p>
+              bamoft integrates the whole infrastructure needed for getting
+              started receiving payments, including a server, components and
+              packages.
+            </p>
+          </div>
+
+          {/* <div>
+            <h2>1. Easy integration with bitcoin network</h2>
+            <p>Understanding how bitcoin network operates is painful enough, bamotf streamline the complexity and made it more digestible.</p>
+          </div>
+
+          <div>
+            <h2>2. Autocomplete and type-safe</h2>
+            <p>The best code is the code that writes itself, so you can move quickly and be sure you don't break stuff accidentally.</p>
+          </div>
+
+          <div>
+            <h2>3. Dev environment</h2>
+            <p>Built with a strong typed programming language, so it doesn't require PhD to learn or xxxxxx.</p>
+          </div>
+
+          <div>
+            <h2>4. xxxxx</h2>
+            <p>Pre-built components and utilities that speed up development and makes it the perfect starting point for your next project.</p>
+          </div>
+
+          <div>
+            <h2>5. Bitcoin for dev couldn’t be easier - or faster</h2>
+            <p>bamotf automates your build, test, and deployment into the 3 Bitcoin network.</p>
+          </div> */}
+        </section>
       </main>
     </>
   )
