08dc4aecc41bfede2a519176d85ce71bed04989f
Wlad Paiva <desenvolvedormg@gmail.com>
2023-07-19 14:47:33 -0300

    update docs

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -2,4 +2,8 @@ const sharedConfig = require('tailwind-config/tailwind.config.js')
 
 module.exports = {
   presets: [sharedConfig],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+  ],
 }
