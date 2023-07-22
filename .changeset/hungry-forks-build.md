656275417feef0eb154831e247d18c10d0cb8f14
Wlad Paiva <desenvolvedormg@gmail.com>
2023-07-22 15:09:58 -0300

    fix: tiny-secp256k1 wasm not found

---
+++ .changeset/hungry-forks-build.md
@@ -0,0 +1,6 @@
+---
+'@bamotf/node': patch
+---
+
+Replaces the `tiny-secp256k1` dependency with the `@bitcoinerlab/secp256k1` to
+be able to support more node environments
--- packages/node/src/address.ts
+++ packages/node/src/address.ts
@@ -1,6 +1,6 @@
+import ecc from '@bitcoinerlab/secp256k1'
 import BIP32Factory from 'bip32'
 import * as bitcoin from 'bitcoinjs-lib'
-import * as ecc from 'tiny-secp256k1'
 
 const bip32 = BIP32Factory(ecc)
 
