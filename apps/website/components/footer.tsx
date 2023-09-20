c2f5aaa3bdfe6ce8045a152f778fe1d24793167d
Wlad Paiva <desenvolvedormg@gmail.com>
2023-09-20 11:50:45 -0300

    rename  app to

--- apps/website/components/footer.tsx
+++ apps/website/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import {links} from '../../donation/config/links'
+import {links} from '../config/links'
 import {Logo} from './logo'
 
 const navLinks = [
