02f60d40ba96f9579a4e98467a1d934188440fd9
Wlad Paiva <desenvolvedormg@gmail.com>
2023-09-28 11:47:03 -0300

    Push fix to build pipeline and add more benefits on website

--- apps/website/components/landing/feature-b.tsx
+++ apps/website/components/landing/feature-b.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {CheckIcon} from 'lucide-react'
 
 const features = [
   {
-    name: 'Asynchronous payments',
+    name: 'Asynchronous checks',
     description:
       'No more background services to monitor transactions - get instant notifications via webhook once payments are confirmed on the blockchain',
+  },
+  {
+    name: 'CI/CD friendly',
+    description:
+      'Deploy your app with confidence. bamotf is designed to be used in CI/CD environments so you can test your application on all levels.',
   },
 ]
 
@@ -16,10 +22,10 @@ export default function FeatureB() {
           <h2 className="text-base font-semibold leading-7 text-orange-400">
             Everything you need
           </h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
             Don&apos;t worry about how Bitcoin operates
           </p>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
+          <p className="mt-6 text-lg leading-8 text-neutral-700 dark:text-neutral-300">
             bamotf handles the communication with the of Bitcoin network. You
             focus on your business logic.
           </p>
@@ -27,9 +33,15 @@ export default function FeatureB() {
 
         <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {features.map(feature => (
-            <div key={feature.name}>
-              <dt className="font-bold">{feature.name}</dt>
-              <dd className="mt-1">{feature.description}</dd>
+            <div key={feature.name} className="relative pl-9">
+              <dt className="font-semibold">
+                <CheckIcon
+                  className="absolute left-0 top-1 h-5 w-5 text-orange-500"
+                  aria-hidden="true"
+                />
+                {feature.name}
+              </dt>
+              <dd className="mt-2">{feature.description}</dd>
             </div>
           ))}
         </dl>
--- apps/website/components/landing/index.tsx
+++ apps/website/components/landing/index.tsx
@@ -14,6 +14,7 @@ export function LandingPage() {
         </div>
 
         <FeatureA />
+        <FeatureB />
 
         <CallToAction />
       </main>
